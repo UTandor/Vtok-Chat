697f902662573fc3cbe938862080830ae0250e08
UTandor <demonsouls34865@gmail.com>
2023-12-21 05:16:40 -0800

    Login, Logout, Delete and Register refactored.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Register from "./components/Register";
-import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
 function App() {
   const navigate = useNavigate();
-  const location = useLocation();
-  const pathname = location;
-
   const name = localStorage.getItem("name");
   const password = localStorage.getItem("password");
 
   useEffect(() => {
-      if (!name && !password && pathname !== '/register') {
-        return (
-          <div>You are not logged in </div>
-        )
+    if (!name || !password) {
+      if (window.location.pathname !== "/register") {
+        navigate("/login");
       }
-  }, []);
+    }
+  }, [name, password, navigate]);
 
   return (
     <Routes>
@@ -30,86 +27,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   const [items, setItems] = useState([]);
-//   const [newItem, setNewItem] = useState("");
-
-//   useEffect(() => {
-//     fetchItems();
-//   }, []);
-
-//   const fetchItems = () => {
-//     axios
-//       .get("http://localhost:8080/items")
-//       .then((response) => {
-//         setItems(response.data);
-//       })
-//       .catch((error) => console.error(error));
-//   };
-
-//   const handleCheck = (id) => {
-//     axios
-//       .put(`http://localhost:8080/items/${id}`)
-//       .then((response) => {
-//         const updatedItems = items.map((item) => {
-//           if (item._id === id) {
-//             return { ...item, completed: !item.completed };
-//           }
-//           return item;
-//         });
-//         setItems(updatedItems);
-//       })
-//       .catch((error) => console.error(error));
-//   };
-
-//   const handleCreate = (e) => {
-//     e.preventDefault();
-//     axios
-//       .post("http://localhost:8080/items", { name: newItem })
-//       .then((response) => {
-//         fetchItems();
-//         setNewItem("");
-//       })
-//       .catch((error) => console.error(error));
-//   };
-
-//   const handleDelete = (id) => {
-//     axios
-//       .delete(`http://localhost:8080/items/${id}`)
-//       .then((response) => {
-//         fetchItems();
-//       })
-//       .catch((error) => console.error(error));
-//   };
-
-//   return (
-//     <div>
-//       <h2>Item Manager</h2>
-//       <form onSubmit={handleCreate}>
-//         <input
-//           type="text"
-//           placeholder="Enter Item Name"
-//           onChange={(e) => setNewItem(e.target.value)}
-//           value={newItem}
-//         />
-//         <input type="submit" value="Add" />
-//       </form>
-//       {items.length === 0 ? (
-//         <div>
-//           <h2>No items</h2>
-//         </div>
-//       ) : (
-//         items.map((item) => (
-//           <div key={item._id}>
-//             <button onClick={() => handleCheck(item._id)}>
-//               {item.completed ? "Done" : "Pending"}
-//             </button>
-//             {item.name}
-//             <button onClick={() => handleDelete(item._id)}>Delete</button>
-//           </div>
-//         ))
-//       )}
-//     </div>
-//   );
-// }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import axios from "axios";
 
 const Home = () => {
-  const name = localStorage.getItem("name");
-  const password = localStorage.getItem("password");
-
   const handleLogout = () => {
-    localStorage.removeItem(name)
-    localStorage.removeItem(password)
-
+    localStorage.removeItem("name");
+    localStorage.removeItem("password");
+    window.location.replace("/login");
   };
 
   const handleDelete = () => {
+    const name = localStorage.getItem("name");
     axios
       .delete(`http://localhost:8080/users/${name}`)
       .then((response) => {
         console.log("User successfully deleted:", response.data);
+        localStorage.removeItem("name");
+        localStorage.removeItem("password");
+        window.location.replace("/login");
       })
       .catch((error) => {
         console.error("Error deleting data:", error);
